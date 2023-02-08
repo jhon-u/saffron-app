033877cf2f05fa05f2fa58c2e19b643bd0a68bcb
Jhon Useche <jhon.useche@telus.com>
2023-02-08 16:04:08 -0700

    Add delete favourite functionality.

--- client-react/src/components/RecipeCard.jsx
+++ client-react/src/components/RecipeCard.jsx
@@ -1,37 +1,34 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import { NavLink } from 'react-router-dom';
-import { useContext } from 'react';
-import { favouritesContext } from 'Providers/FavouritesProvider';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { favouritesContext } from "Providers/FavouritesProvider";
 
 export default function RecipeCard(props) {
-  const [expanded, setExpanded] = React.useState(false);
-  const [open, setOpen] = React.useState(false);
-  const [data, setData] = React.useState({});
-  const { favourites, setFavourites, saveFavourites, isFavourite, deleteFavourite } = useContext(favouritesContext)
-  
+  const { saveFavourites, isFavourite, deleteFavourite } =
+    useContext(favouritesContext);
+
   const handleFavouriteState = (recipeid) => {
-    console.log('RECIPE ID', recipeid)
-    if (isFavourite(props.id)) {
-      deleteFavourite(recipeid)     
+    if (isFavourite(recipeid)) {
+      deleteFavourite(recipeid);
+    } else {
+      saveFavourites({
+        recipeid: props.id,
+        title: props.title,
+        image: props.image,
+      });
     }
-    else {
-      saveFavourites({recipeid: props.id, title: props.title, image: props.image })
-    }
+  };
 
-
-
-  }
-  
   return (
     <Card sx={{ maxWidth: 300 }}>
       <CardHeader
@@ -43,19 +40,20 @@ export default function RecipeCard(props) {
         title={props.title}
       />
       <NavLink to={`/receipes/${props.id}`} state={props.id}>
-        <CardMedia 
-          onClick={() => console.log('Image clicked')}
+        <CardMedia
           component="img"
           height="194"
           image={props.image}
           alt={props.title}
         />
       </NavLink>
-      <CardContent>
-      </CardContent>
+      <CardContent></CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={() => handleFavouriteState(props.id)}>
-          {isFavourite(props.id) && <FavoriteIcon style={{ color: 'red' }}/>}
+        <IconButton
+          aria-label="add to favorites"
+          onClick={() => handleFavouriteState(props.id)}
+        >
+          {isFavourite(props.id) && <FavoriteIcon style={{ color: "red" }} />}
           {!isFavourite(props.id) && <FavoriteIcon />}
         </IconButton>
         <IconButton aria-label="share">
@@ -63,6 +61,5 @@ export default function RecipeCard(props) {
         </IconButton>
       </CardActions>
     </Card>
-
   );
 }
