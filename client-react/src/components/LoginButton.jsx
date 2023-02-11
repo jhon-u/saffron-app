c070ec8e8f069bb4bbe320d1ee42dfd3d4c91b58
Tony Eng <tony.eng@telus.com>
2023-02-10 19:32:58 -0800

    fix bug logout clears favourites context

--- client-react/src/components/LoginButton.jsx
+++ client-react/src/components/LoginButton.jsx
@@ -2,11 +2,17 @@ import { Button } from "@mui/material"
 import { useContext, useState } from "react"
 import { Link } from "react-router-dom"
 import { authContext } from "Providers/AuthProvider"
+import { favouritesContext } from "Providers/FavouritesProvider";
 
 
 export default function LoginButton(props) {
   const { auth, user, login, logout } = useContext(authContext)
+  const { setFavourites } = useContext(favouritesContext)
 
+  const handleClick = () => {
+    setFavourites([]);
+    logout();
+  }
   return (
     <>
       {!auth &&
@@ -15,7 +21,7 @@ export default function LoginButton(props) {
         </Button>
       }
       {auth &&
-        <Button variant="contained" onClick={logout} component={Link} to={"/"}>
+        <Button variant="contained" onClick={handleClick} component={Link} to={"/"}>
           Logout
         </Button>
       }
--- client-react/src/pages/MealPlanner.jsx
+++ client-react/src/pages/MealPlanner.jsx
@@ -7,29 +7,33 @@ import { StrictModeDroppable as Droppable } from "helpers/StrictMode.Droppable";
 import { useContext, useNavigate } from "react";
 import { favouritesContext } from "Providers/FavouritesProvider";
 import RecipeCard from "components/RecipeCard";
+import { authContext } from "Providers/AuthProvider"
+import LoginForm from "components/LoginForm";
 
 export default function MealPlanner() {
   const { favourites, setFavourites, getFavourites } = useContext(favouritesContext)
   const [loading, setLoading] = useState(false);
   const [meals, setMeals] = useState(favourites);
+  const { auth, user } = useContext(authContext)
 
   const getRecipeId = (id) => {
     setRecipeId(id)
   }
-
-  useEffect(() => {
-    axios.get(`/favourites`)
-      .then((res) => {
-        console.log("is this hitting again for axios?")
-        setFavourites(res.data);
-        setLoading(true);
-      })
-      .catch((err) => {
-        setFavourites({ error: err.message });
-      });
-  }, []);
+  
+  // useEffect(() => {
+  //   axios.get(`/favourites`)
+  //   .then((res) => {
+  //     console.log("is this hitting again for axios?")
+  //     setFavourites(res.data);
+  //     setLoading(true);
+  //   })
+  //   .catch((err) => {
+  //     setFavourites({ error: err.message });
+  //   });
+  // }, []);
+  
   console.log("meals check", meals)
-  const displayFavourites = favourites?.map((recipe, index) => {
+  const displayFavourites = meals?.map((recipe, index) => {
     return (
       <Draggable key={recipe.id} draggableId={recipe.id.toString()} index={index}>
         {(provided) => (
@@ -48,15 +52,16 @@ export default function MealPlanner() {
   });
 
   function handleOnDragEnd(result) {
-    const items = Array.from(favourites);
+    const items = Array.from(meals);
     const [reorderedItem] = items.splice(result.source.index, 1);
     items.splice(result.destination.index, 0, reorderedItem)
-    setFavourites(items);
+    setMeals(items);
     console.log(result)
   }
 
   return (
     <>
+      {auth && (
       <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId="meals">
           {(provided) => (
@@ -67,6 +72,12 @@ export default function MealPlanner() {
           )}
         </Droppable>
       </DragDropContext>
+      )}
+      {!auth && (
+        <div className="recipeList">
+          <LoginForm path={"/favourites"}/>
+        </div>
+      )}
     </>
   )
 }
