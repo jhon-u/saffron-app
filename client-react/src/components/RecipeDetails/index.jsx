71ed38a7f58cada9cabd9c527bea69d9a4b18473
Jhon Useche <jhon.useche@telus.com>
2023-02-11 17:45:57 -0700

    Override the image fit in the card media container.

--- client-react/src/components/RecipeDetails/index.jsx
+++ client-react/src/components/RecipeDetails/index.jsx
@@ -83,20 +83,21 @@ export default function RecipeDetails(props) {
       <Button variant="contained" onClick={() => navigate(-1)} style={{ marginBottom: "20px" }}>Back</Button>
       <Grid container spacing={2}>
         {/* Header left, aka Image */}
-        <Grid container item xs={12} md={4} lg={2}>
+        <Grid container item xs={12} md={4} lg={4}>
           <Grid item xs={12} md={12} lg={12}>
             <CardMedia
               component="img"
-              height="240"
-              width="240"
+              height="400"
+              width="400"
               image={recipeDetails.image}
               alt={props.title}
+              sx={{ objectFit: "contain", borderRadius: 12 }}
             />
           </Grid>
         </Grid>
 
         {/* Header right */}
-        <Grid container item xs={12} md={8} lg={10}>
+        <Grid container item xs={12} md={8} lg={8}>
           <Grid item xs={12} md={12} lg={12}>
             <Title />
             <RandomStarRating />
