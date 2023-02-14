f880039d60bc33d5558c6f36dbc77f10209e5aad
Jhon Useche <jhon.useche@telus.com>
2023-02-13 20:23:59 -0700

    Fix padding left in the instruction component.

--- client-react/src/components/RecipeDetails/Instructions.jsx
+++ client-react/src/components/RecipeDetails/Instructions.jsx
@@ -1,39 +1,32 @@
-import { React, useContext } from "react"
-import { Typography } from "@mui/material"
+import { React, useContext } from "react";
+import { Typography } from "@mui/material";
 import { recipeDetailsContext } from "../../Providers/RecipeDetailsProvider";
-import { Grid } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 
 export default function Instructions(props) {
-
-  const { recipeDetails } = useContext(recipeDetailsContext)
+  const { recipeDetails } = useContext(recipeDetailsContext);
 
   const steps = recipeDetails.instructions?.map((step, index) => {
     return (
       <li key={index}>
         <Typography
-            variant="body1"
-            sx={{ textAlign: "justify", textJustify: "inter-word" }}
-          >
+          variant="body1"
+          sx={{ textAlign: "justify", textJustify: "inter-word" }}
+        >
           {step}
         </Typography>
       </li>
-    ) 
-    
-    
-    
-    
+    );
   });
 
   return (
     <Grid item xs={11} md={11} lg={11}>
-      <Typography 
-        variant="h4"
-        sx={{ paddingBottom: "12px"}}
-      >
+      <Typography variant="h4" sx={{ paddingBottom: "12px" }}>
         Instructions
       </Typography>
-     <ol>{steps}</ol>
+      <Box sx={{pl: 2.5}}>
+        <ol>{steps}</ol>
+      </Box>
     </Grid>
   );
 }
-
