3f58934fba79cf7242969d3bf2f232374227962b
Jhon Useche <jhon.useche@telus.com>
2023-02-16 10:07:29 -0700

    Add skeleton component to show while data is loading.

---
+++ client-react/src/components/RecipeDetails/Rating.jsx
@@ -0,0 +1,24 @@
+import { useState ,useContext, useRef, useEffect } from "react"
+import { Rating, Box } from "@mui/material"
+
+
+export default function RandomStarRating() {
+  const [starRating, setStarRating] = useState(Math.floor(Math.random() * 5) + 1);
+
+  const usePrevious = (value) => {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    }, [value]);
+
+    return ref.current;
+  };
+
+  const previousRating = usePrevious(starRating);
+
+  return (
+    <Box sx={{ "& > legend": { mt: 2 } }}>
+      <Rating name="read-only" value={previousRating} readOnly />
+    </Box>
+  );
+}
--- client-react/src/components/RecipeDetails/Title.jsx
+++ client-react/src/components/RecipeDetails/Title.jsx
@@ -1,18 +1,23 @@
-import { React ,useContext } from "react"
-import { Typography } from "@mui/material"
+import { React, useContext } from "react";
+import { Typography, Box, Skeleton } from "@mui/material";
 import { recipeDetailsContext } from "../../Providers/RecipeDetailsProvider";
-
+import { loadingContext } from "../../Providers/LoadingProvider";
 
 export default function Title(props) {
-  const { recipeDetails } = useContext(recipeDetailsContext)
+  const { recipeDetails } = useContext(recipeDetailsContext);
+  const { loading } = useContext(loadingContext);
 
   return (
-    <Typography 
-    variant="h4"
-    letterSpacing={1}
-    textTransform='uppercase'
-    >
-      {recipeDetails.title}
-    </Typography>
+    <>
+      {loading ? (
+        <Box sx={{ width: 700 }}>
+          <Skeleton />
+        </Box>
+      ) : (
+        <Typography variant="h4" letterSpacing={1} textTransform="uppercase">
+          {recipeDetails.title}
+        </Typography>
+      )}
+    </>
   );
 }
